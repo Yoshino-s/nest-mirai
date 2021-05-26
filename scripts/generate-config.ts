9ad3fd766cfe9d991fc827e265ddfa09466bdae9
Yoshino-s <yoshino.prog@gmail.com>
2021-05-27 00:55:31 +0800

    feat(mirai): add device.json support

--- scripts/generate-config.ts
+++ scripts/generate-config.ts
@@ -36,6 +36,10 @@ async function main() {
       return dump(config);
     }),
   ]);
+  if(fs.existsSync("./device.json")) {
+    fs.mkdirSync(`./mirai/src/bots/${yamlConfig.qq}`, {recursive: true});
+    fs.copyFileSync("./device.json", `./mirai/src/bots/${yamlConfig.qq}/device.json`);
+  }
 }
 
 main();
